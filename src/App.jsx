9d1e767bcae91cdbe886b118280ef3918d5f0e8b
Upreti Bimesh <s2400076@BC-C02GR0C3Q6LRs-MacBook-Air.local>
2024-06-03 01:52:24 +0300

    Changes in Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,64 @@
+// import "./App.css";
+// import { useState } from "react";
+// import { RouterProvider, createBrowserRouter } from "react-router-dom";
+// import Root from "./routes/Root.jsx";
+// import Home from "./routes/Home.jsx";
+// import About from "./routes/About.jsx";
+// import SinglePage from "./routes/SinglePage.jsx";
+// import CategoryPage from "./routes/CategoryPage.jsx";
+// import ErrorPage from "./routes/ErrorPage.jsx";
+// import { animals, birds, insects, fishes } from "./animalsList";
+
+// function App() {
+//     const [zoo, setZoo] = useState({ animals, birds, insects, fishes });
+
+//     const removeHandler = (name, category) => {
+//         setZoo((prevZoo) => ({
+//             ...prevZoo,
+//             [category]: prevZoo[category].filter((el) => el.name !== name),
+//         }));
+//     };
+
+//     const likesHandler = (name, category, action) => {
+//         setZoo((prevZoo) => ({
+//             ...prevZoo,
+//             [category]: prevZoo[category].map((el) =>
+//                 el.name === name ? { ...el, likes: el.likes + (action === 'add' ? 1 : -1) } : el
+//             ),
+//         }));
+//     };
+
+//     const router = createBrowserRouter([
+//         { path: "/", element: <Home /> },
+//         {
+//             path: "/",
+//             element: <Root />,
+//             errorElement: <ErrorPage />,
+//             children: [
+//                 {
+//                     path: ":category",
+//                     element: <CategoryPage
+//                         addLikes={likesHandler}
+//                         removeCard={removeHandler}
+//                         removeLikes={likesHandler}
+//                         {...zoo}
+//                     />,
+//                 },
+//                 { path: ':category/:name', element: <SinglePage {...zoo} /> },
+//                 { path: "about", element: <About /> },
+//             ],
+//         },
+//     ]);
+
+//     return <RouterProvider router={router} />;
+// }
+
+// export default App;
+
+
 import "./App.css";
 import { useState } from "react";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Root from "./routes/Root.jsx";
 import Home from "./routes/Home.jsx";
 import About from "./routes/About.jsx";
@@ -28,29 +86,25 @@ function App() {
         }));
     };
 
-    const router = createBrowserRouter([
-        { path: "/", element: <Home /> },
-        {
-            path: "/",
-            element: <Root />,
-            errorElement: <ErrorPage />,
-            children: [
-                {
-                    path: ":category",
-                    element: <CategoryPage
-                        addLikes={likesHandler}
-                        removeCard={removeHandler}
-                        removeLikes={likesHandler}
-                        {...zoo}
-                    />,
-                },
-                { path: ':category/:name', element: <SinglePage {...zoo} /> },
-                { path: "about", element: <About /> },
-            ],
-        },
-    ]);
-
-    return <RouterProvider router={router} />;
+    return (
+        <Router basename="/zoo_app">
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/" element={<Root />} errorElement={<ErrorPage />}>
+                    <Route path=":category" element={
+                        <CategoryPage
+                            addLikes={likesHandler}
+                            removeCard={removeHandler}
+                            removeLikes={likesHandler}
+                            {...zoo}
+                        />
+                    } />
+                    <Route path=":category/:name" element={<SinglePage {...zoo} />} />
+                    <Route path="about" element={<About />} />
+                </Route>
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
